906bf3d1aa9b4d1296e76460da21681f84749a7c
vedang29 <vedang294@gmail.com>
2025-07-30 08:02:12 +0530

    Use process.env.PORT for Render deployment

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 4000;
+// const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
--- server.js
+++ server.js
@@ -3,7 +3,8 @@ import bodyParser from "body-parser";
 import axios from "axios";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+// const port = 3000;
 const API_URL = "http://localhost:4000"
 
 app.use(bodyParser.urlencoded({ extended: true }));
